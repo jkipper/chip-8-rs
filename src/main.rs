5556af8d0c31a7561e8f61b6cc2abcca293d6d3e
jkipper <justin.kipper94@gmail.com>
2023-01-21 23:25:06 +0100

    Check opcode parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use std::{fs, slice, path::Path};
+
 #[derive(Debug)]
 struct Address(u16);
+
+impl From<&[u8; 3]> for Address {
+    fn from(addr: &[u8; 3]) -> Address {
+        return Address(((addr[0] as u16) << 8 | (addr[1] as u16) << 4 | addr[2] as u16));
+    }
+}
+
 #[derive(Debug)]
 struct Const8(u8);
+
+impl From<&[u8; 2]> for Const8 {
+    fn from(data: &[u8; 2]) -> Const8 {
+        return Const8(data[0 << 4] | data[1]);
+    }
+}
+
 #[derive(Debug)]
 struct Const4(u8);
 #[derive(Debug)]
@@ -53,12 +69,15 @@ static STACK: [u16; 16] = [0; 16];
 fn main() {
     let pc: usize = 0x00;
     let sp: u16 = 0x00;
-    println!("Hello, world!");
-
-    fetch_opcode(&MEMORY[pc as usize..pc as usize + 1]);
+    let test_path = Path::new("../chip8-test-rom/chip8-test-rom.8o");
+    let contents = fs::read(test_path).expect("Should be able to open the file");
+    let _opcodes: Vec<OpCode> = contents
+        .chunks_exact(2)
+        .filter_map(decode_opcode)
+        .collect();
 }
 
-fn fetch_opcode(opc: &[u8]) -> Option<OpCode> {
+fn decode_opcode(opc: &[u8]) -> Option<OpCode> {
     let parts = [
         (opc[0] & 0xF0) >> 4,
         opc[0] & 0x0F,
@@ -68,14 +87,14 @@ fn fetch_opcode(opc: &[u8]) -> Option<OpCode> {
     return match parts {
         [0x0, 0x0, 0xE, 0xE] => Some(OpCode::Ret()),
         [0x0, 0x0, 0xE, 0x0] => Some(OpCode::Clear()),
-        [0x0, ref rest @ ..] => Some(OpCode::CallRoutine(to_addr(rest))),
-        [0x1, ref rest @ ..] => Some(OpCode::Jmp(to_addr(rest))),
-        [0x2, ref rest @ ..] => Some(OpCode::CallSubroutine(to_addr(rest))),
-        [0x3, x, ref rest @ ..] => Some(OpCode::SkipConstEq(Var(x), to_const(rest))),
-        [0x4, x, ref rest @ ..] => Some(OpCode::SkipConstNe(Var(x), to_const(rest))),
+        [0x0, ref addr @ ..] => Some(OpCode::CallRoutine(Address::from(addr))),
+        [0x1, ref addr @ ..] => Some(OpCode::Jmp(Address::from(addr))),
+        [0x2, ref addr @ ..] => Some(OpCode::CallSubroutine(Address::from(addr))),
+        [0x3, x, ref rest @ ..] => Some(OpCode::SkipConstEq(Var(x), Const8::from(rest))),
+        [0x4, x, ref rest @ ..] => Some(OpCode::SkipConstNe(Var(x), Const8::from(rest))),
         [0x5, x, y, 0x0] => Some(OpCode::SkipEq(Var(x), Var(y))),
-        [0x6, x, ref rest @ ..] => Some(OpCode::SetConst(Var(x), to_const(rest))),
-        [0x7, x, ref rest @ ..] => Some(OpCode::AddConst(Var(x), to_const(rest))),
+        [0x6, x, ref rest @ ..] => Some(OpCode::SetConst(Var(x), Const8::from(rest))),
+        [0x7, x, ref rest @ ..] => Some(OpCode::AddConst(Var(x), Const8::from(rest))),
         [0x8, x, y, 0x0] => Some(OpCode::Set(Var(x), Var(y))),
         [0x8, x, y, 0x1] => Some(OpCode::BitOR(Var(x), Var(y))),
         [0x8, x, y, 0x2] => Some(OpCode::BitAND(Var(x), Var(y))),
@@ -86,9 +105,9 @@ fn fetch_opcode(opc: &[u8]) -> Option<OpCode> {
         [0x8, x, y, 0x7] => Some(OpCode::SubReg(Var(x), Var(y))),
         [0x8, x, y, 0xE] => Some(OpCode::BitLShift(Var(x), Var(y))),
         [0x9, x, y, 0x0] => Some(OpCode::SkipNe(Var(x), Var(y))),
-        [0xA, ref addr @ ..] => Some(OpCode::IToAddr(to_addr(addr))),
-        [0xB, ref addr @ ..] => Some(OpCode::SetPc(to_addr(addr))),
-        [0xC, x, ref c8 @ ..] => Some(OpCode::Rand(Var(x), to_const(c8))),
+        [0xA, ref addr @ ..] => Some(OpCode::IToAddr(Address::from(addr))),
+        [0xB, ref addr @ ..] => Some(OpCode::SetPc(Address::from(addr))),
+        [0xC, x, ref c8 @ ..] => Some(OpCode::Rand(Var(x), Const8::from(c8))),
         [0xD, x, y, c] => Some(OpCode::Draw(Var(x), Var(y), Const4(c))),
         [0xE, x, 0x9, 0xE] => Some(OpCode::KeyEq(Var(x))),
         [0xE, x, 0xA, 0x1] => Some(OpCode::KeyNe(Var(x))),
@@ -101,15 +120,9 @@ fn fetch_opcode(opc: &[u8]) -> Option<OpCode> {
         [0xF, x, 0x3, 0x3] => Some(OpCode::StoreBCD(Var(x))),
         [0xF, x, 0x5, 0x5] => Some(OpCode::RegDump(Var(x))),
         [0xF, x, 0x6, 0x5] => Some(OpCode::RegLoad(Var(x))),
-        _ => None,
+        _ => {
+            println!("Unkown opcode for {:x?}", parts);
+            None
+        }
     };
-}
-fn to_const(opc: &[u8]) -> Const8 {
-    return Const8(opc[0 << 4] | opc[1]);
-}
-
-fn to_addr(opc: &[u8]) -> Address {
-    return Address(
-        (opc[0] as u16) << 12 | (opc[1] as u16) << 8 | (opc[2] as u16) << 4 | opc[3] as u16,
-    );
 }
